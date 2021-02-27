f949eb08f316d12bb05eb10636450ef17a0644c9
Sameer <sameer@umd.edu>
2021-02-27 17:42:33 -0500

    Added Pygame

--- Project2/Dijkstra.py
+++ Project2/Dijkstra.py
@@ -1,5 +1,6 @@
 import heapq
 import pygame
+import sys
 
 class Node:
 
@@ -18,18 +19,23 @@ class Graph:
     def __init__(self):
         self.height = 300
         self.width = 400
-        self.gridDisplay = pygame.display.set_mode((400, 300))
+
+        self.CELL_MARGIN = .5
+        self.CELL_WIDTH = 2
+        self.CELL_HEIGHT = 2
 
     def generateGraph(self):
 
         for i in range(self.height):
             for j in range(self.width):
                 currentNode = Node(i,j)
-                pygame.draw.rect(self.gridDisplay, color, [i, j, 10, 10 ])
+                # pygame.draw.rect(gridDisplay, (0,0,150), (j, i, 10, 10 ),2)
+                pygame.draw.rect(gridDisplay,(255,255,255),[(self.CELL_MARGIN + self.CELL_WIDTH) * j + self.CELL_MARGIN,(self.CELL_MARGIN + self.CELL_HEIGHT) * i + self.CELL_MARGIN,self.CELL_WIDTH, self.CELL_HEIGHT])
+                pygame.display.update()
                 neighbours = self.getNeighbours(currentNode)
+
                 #Add Neighbours to each node
                 #Node(currentNode.addNeighbours)
-        pygame.display.update()
 
     def getNeighbours(self, currentNode):
         i, j = currentNode.i, currentNode.j
@@ -98,12 +104,66 @@ class Graph:
     def backTrack(self, end):
         pass
 
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+grid = []
+for row in range(300):
+    grid.append([])
+    for column in range(400):
+        grid[row].append(0)
+
+pygame.init()
+gridDisplay = pygame.display.set_mode((400, 300))
+pygame.display.set_caption("First Game")
+
+done = False
+clock = pygame.time.Clock()
 
 obj = Graph()
 obj.generateGraph()
-
 start = Node(0,0)
-end = Node(300,400)
+end = Node(600,800)
 robot = Graph()
-# robot.performDijkstra(start, end)
+robot.performDijkstra(start, end)
 
+while not done:
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
+        # elif event.type == pygame.MOUSEBUTTONDOWN:
+        #     # User clicks the mouse. Get the position
+        #     pos = pygame.mouse.get_pos()
+        #     # Change the x/y screen coordinates to grid coordinates
+        #     column = pos[0] // (WIDTH + MARGIN)
+        #     row = pos[1] // (HEIGHT + MARGIN)
+        #     # Set that location to one
+        #     grid[row][column] = 1
+        #     print("Click ", pos, "Grid coordinates: ", row, column)
+ 
+    # Set the screen background 
+    # Draw the grid
+    # for row in range(10):
+    #     for column in range(10):
+    #         color = WHITE
+    #         if grid[row][column] == 1:
+    #             color = GREEN
+
+ 
+    clock.tick(144)
+ 
+    pygame.display.flip()
+ 
+pygame.quit()
+
+
+
+
+# while True:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#             sys.exit()
+    
