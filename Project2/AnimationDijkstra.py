dfb74b9a22e30496ab360eb5aab756dbf9273fc1
Sameer <sameer@umd.edu>
2021-03-01 13:05:00 -0500

    Choppy Animation Added

--- Project2/AnimationDijkstra.py
+++ Project2/AnimationDijkstra.py
@@ -1,6 +1,9 @@
 import pygame
 import sys
 
+HEIGHT = 300
+WIDTH = 400
+
 class Node:
 
     def __init__(self, i ,j):
@@ -12,8 +15,6 @@ class Node:
 
 class Graph:
     def __init__(self):
-        self.height= 300
-        self.width = 400
 
         self.CELL_MARGIN = .5
         self.CELL_WIDTH = 2
@@ -45,22 +46,22 @@ class Graph:
             neighbours[tuple([i,j-1])] = 1
         
         #Top
-        if i < (self.height -1) and (not self.isAnObstacle(i+1, j)):
+        if i < (HEIGHT -1) and (not self.isAnObstacle(i+1, j)):
             #print("No Obstacle found at ", i+1, j)
             neighbours[tuple([i+1,j])] = 1
         
         #Right
-        if j < (self.width -1) and (not self.isAnObstacle(i, j+1)):
+        if j < (WIDTH -1) and (not self.isAnObstacle(i, j+1)):
             #print("No Obstacle found at ", i, j+1)
             neighbours[tuple([i,j+1])] = 1
         
         #TopLeft 
-        if j > 0 and i < (self.height-1) and (not self.isAnObstacle(i+1, j-1)):
+        if j > 0 and i < (HEIGHT-1) and (not self.isAnObstacle(i+1, j-1)):
             #print("No Obstacle found at ", i+1, j-1)
             neighbours[tuple([i+1, j-1])] = 1.41
         
         #TopRight 
-        if j < (self.width-1) and i < (self.height-1)and (not self.isAnObstacle(i+1, j+1)):
+        if j < (WIDTH-1) and i < (HEIGHT-1)and (not self.isAnObstacle(i+1, j+1)):
             #print("No Obstacle found at ", i+1, j+1)
             neighbours[tuple([i+1, j+1])] = 1.41
         
@@ -70,7 +71,7 @@ class Graph:
             neighbours[tuple([i-1, j-1])] = 1.41
 
         #BottomRight 
-        if i > 0 and j < (self.width -1) and (not self.isAnObstacle(i-1, j+1)):
+        if i > 0 and j < (WIDTH -1) and (not self.isAnObstacle(i-1, j+1)):
             #print("No Obstacle found at ", i-1, j+1)
             neighbours[tuple([i-1, j+1])] = 1.41
         
@@ -90,7 +91,6 @@ class Graph:
             print("Ending point is inside the obstacle!")
             return 
 
-
         priorityQueue = [start]
         while len(priorityQueue):
             priorityQueue.sort(key = lambda x: x.distanceToReach)
@@ -103,8 +103,13 @@ class Graph:
                  
             currentDistance = currentNode.distanceToReach
             neighbours = self.getNeighbours(currentNode)
+
             for neighbour, newDistance in neighbours.items():
-                neighbourNode = Node(neighbour[0], neighbour[1])
+                i = neighbour[0]
+                j = neighbour[1]
+                neighbourNode = Node(i,j)
+                pygame.draw.rect(gridDisplay,(255,255,255),[(self.CELL_MARGIN + self.CELL_WIDTH) * j + self.CELL_MARGIN,(self.CELL_MARGIN + self.CELL_HEIGHT) * i + self.CELL_MARGIN,self.CELL_WIDTH, self.CELL_HEIGHT])
+                pygame.display.update()
                 neighbourNode.distanceToReach = currentDistance + newDistance
                 neighbourNode.parent = currentNode
                 priorityQueue.append(neighbourNode)
@@ -114,6 +119,7 @@ class Graph:
     def backTrack(self, child):
         while child != None:
             print(child.i, child.j)
+            grid[child.i][child.j] = 1
             child = child.parent
         return True
     
@@ -167,12 +173,31 @@ class Graph:
         # return self.isInTestCircle(x,y) or self.isInTestCircleTwo(x,y)
         return self.isInEllipse(x,y) or self.isInBrokenRectangle(x,y) or self.isInCircle(x,y) or self.isInRectangle(x,y) or self.isInPolygon(x,y)
 
-i1 = int(input("Enter the ith coordiante of the starting point: "))
-j1 = int(input("Enter the jth coordiante of the starting point: "))
+# i1 = int(input("Enter the ith coordiante of the starting point: "))
+# j1 = int(input("Enter the jth coordiante of the starting point: "))
 
-i2 = int(input("Enter the ith coordiante of the ending point: "))
-j2 = int(input("Enter the jth coordiante of the ending point: "))
+# i2 = int(input("Enter the ith coordiante of the ending point: "))
+# j2 = int(input("Enter the jth coordiante of the ending point: "))
 
+i1 = 140
+j1 = 40
+
+i2 = 140
+j2 = 41
+
+pygame.init()
+gridDisplay = pygame.display.set_mode((1000, 1000 ))
+pygame.display.set_caption("Dijkstra's Algorithm")
+done = False
+clock = pygame.time.Clock()
+
+grid = []
+for row in range(300):
+    grid.append([])
+    for column in range(400):
+        grid[row].append(0)
+
+#Algorithm Driver   
 start = Node(i1,j1)
 start.distanceToReach = 0
 end = Node(i2,j2)
@@ -180,30 +205,16 @@ robot = Graph()
 robot.performDijkstra(start, end)
 
 #PyGame Code
-# BLACK = (0, 0, 0)
-# WHITE = (255, 255, 255)
-# GREEN = (0, 255, 0)
-# RED = (255, 0, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
-# grid = []
-# for row in range(300):
-#     grid.append([])
-#     for column in range(5):
-#         grid[row].append(0)
-
-# pygame.init()
-# gridDisplay = pygame.display.set_mode((100, 100))
-# pygame.display.set_caption("First Game")
-
-# done = False
-# clock = pygame.time.Clock()
-
-# while not done:
-#     for event in pygame.event.get():  # User did something
-#         if event.type == pygame.QUIT:  # If user clicked close
-#             done = True  # Flag that we are done so we exit this loop
+while not done:
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            done = True  # Flag that we are done so we exit this loop
 #         # elif event.type == pygame.MOUSEBUTTONDOWN:
-#         #     # User clicks the mouse. Get the position
 #         #     pos = pygame.mouse.get_pos()
 #         #     # Change the x/y screen coordinates to grid coordinates
 #         #     column = pos[0] // (WIDTH + MARGIN)
@@ -211,17 +222,15 @@ robot.performDijkstra(start, end)
 #         #     # Set that location to one
 #         #     grid[row][column] = 1
 #         #     print("Click ", pos, "Grid coordinates: ", row, column)
+
+    for row in range(HEIGHT):
+        for column in range(WIDTH):
+            color = WHITE
+            if grid[row][column] == 1:
+                color = GREEN
+    clock.tick(144)
  
-#     # Set the screen background 
-#     # Draw the grid
-#     # for row in range(10):
-#     #     for column in range(10):
-#     #         color = WHITE
-#     #         if grid[row][column] == 1:
-#     #             color = GREEN
-#     clock.tick(144)
+    pygame.display.flip()
  
-#     pygame.display.flip()
- 
-# pygame.quit()
+pygame.quit()
 
