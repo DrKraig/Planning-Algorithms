92e9589377c6c8ac141736398b46e9837a4092f8
DrKraig <79610396+DrKraig@users.noreply.github.com>
2021-04-04 17:46:24 -0400

    Update AStarWithNewActionSet.py
    - I've updated the functions isInEllipse(), isInBrokenRectangle(), isInCircle(), isInRectangle() for accomadating clearance
    - isOutsideArena() is also updated for new boundaries
    - I found that while computing obstacle space isOutsideArena() is not being called. Wont that create an issue?

--- Project3/Phase 2/AStarWithNewActionSet.py
+++ Project3/Phase 2/AStarWithNewActionSet.py
@@ -1,5 +1,5 @@
-#https://github.com/SamPusegaonkar/ENPM661/tree/main/Project3
-#Output Videos can be found in the 'Output Videos' folder.
+# https://github.com/SamPusegaonkar/ENPM661/tree/main/Project3
+# Output Videos can be found in the 'Output Videos' folder.
 
 import pygame
 import math
@@ -7,21 +7,23 @@ import heapq
 import time
 import functools
 
-#Defining Graph Constants
+# Defining Graph Constants
 HEIGHT = 300
 WIDTH = 400
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
-MAGENTA = (255,0,255)
+MAGENTA = (255, 0, 255)
+
 
 class Node:
     """
     Node class : This class is built to store the node information.
     A node is simply a location on a map. For each node, its neighbours, parents & distance to reach that node is stored.
     """
-    def __init__(self, i ,j, endI, endJ, theta):
+
+    def __init__(self, i, j, endI, endJ, theta):
         """
         Description: Defining all properties for each node - Neighbours, Parents, Distance.
         """
@@ -29,7 +31,7 @@ class Node:
         self.j = j
         self.theta = theta
         self.costToCome = float('inf')
-        self.costToGo = math.sqrt((i - endI)**2 + (j - endJ)**2)
+        self.costToGo = math.sqrt((i - endI) ** 2 + (j - endJ) ** 2)
         self.cost = None
         self.neighbours = {}
         self.parent = None
@@ -37,10 +39,12 @@ class Node:
     def __lt__(self, other):
         return self.cost < other.cost
 
+
 class Graph:
     """
     Graph class : This class defines all methods to generate a graph and perform AStar Algorithm.
     """
+
     def __init__(self, start, end, MAGNITUDE):
         self.visited = {}
         self.endI = end.i
@@ -52,12 +56,12 @@ class Graph:
         Description: Returns neighbours for the currentNode.
         """
         i, j, theta = currentNode.i, currentNode.j, currentNode.theta
-        neighbours ={}
+        neighbours = {}
         RADIUS = 10
 
         for deltaTheta in range(-60, 60, 30):
-            x, y, newTheta = self.getNewCoordiantes(i, j , theta, deltaTheta)
-            if (not self.isOutsideArena(x,y)) and (not self.isAnObstacle(x,y)):
+            x, y, newTheta = self.getNewCoordiantes(i, j, theta, deltaTheta)
+            if (not self.isOutsideArena(x, y)) and (not self.isAnObstacle(x, y)):
                 newNode = Node(x, y, self.endI, self.endJ, newTheta)
                 neighbours[newNode] = 1
         return neighbours
@@ -68,11 +72,11 @@ class Graph:
         if newTheta > 0:
             newTheta = newTheta % 360
         elif newTheta < 0:
-            newTheta = (newTheta+360) % 360
+            newTheta = (newTheta + 360) % 360
 
-        newI = i + self.MAGNITUDE * math.cos( (math.pi/180) * newTheta)
-        newJ = j + self.MAGNITUDE * math.sin( (math.pi/180) * newTheta)
-        
+        newI = i + self.MAGNITUDE * math.cos((math.pi / 180) * newTheta)
+        newJ = j + self.MAGNITUDE * math.sin((math.pi / 180) * newTheta)
+
         newI = self.getRoundedNumber(newI)
         newJ = self.getRoundedNumber(newJ)
 
@@ -85,36 +89,41 @@ class Graph:
             if decimal >= 0.75:
                 return math.ceil(i)
             else:
-                return int(i) +.5 
+                return int(i) + .5
         elif decimal < 0.5:
             if decimal < 0.25:
                 return math.floor(i)
             else:
-                return int(i) +.5 
-        return i 
-    def generateGraph(self,):
+                return int(i) + .5
+        return i
+
+    def generateGraph(self, ):
         """
-        Description: Checks if a point is in the Ellipse. 
+        Description: Checks if a point is in the Ellipse.
         Input: Point with co-ordinates (x,y)
         Output: True or False
         """
 
-        #Make background White
+        # Make background White
         gridDisplay.fill(WHITE)
 
-        #Circle
-        pygame.draw.circle(gridDisplay, MAGENTA, [90,HEIGHT - 70], 35)
-        
-        #Ellipse
+        # Circle
+        pygame.draw.circle(gridDisplay, MAGENTA, [90, HEIGHT - 70], 35)
+
+        # Ellipse
         pygame.draw.ellipse(gridDisplay, MAGENTA, [186, HEIGHT - 176, 120, 60], 0)
 
-        #Roatated Rect
-        pygame.draw.polygon(gridDisplay, MAGENTA, [(36, HEIGHT - 124), (160, HEIGHT -210), (170, HEIGHT -194),(48,HEIGHT -108)])
+        # Roatated Rect
+        pygame.draw.polygon(gridDisplay, MAGENTA,
+                            [(36, HEIGHT - 124), (160, HEIGHT - 210), (170, HEIGHT - 194), (48, HEIGHT - 108)])
 
-        #Broken Rect
-        pygame.draw.polygon(gridDisplay, MAGENTA, [(200, HEIGHT - 280), (230, HEIGHT -280), (230, HEIGHT -270),(200,HEIGHT -270)])
-        pygame.draw.polygon(gridDisplay, MAGENTA, [(200, HEIGHT - 270), (210, HEIGHT -270), (210, HEIGHT -240),(200,HEIGHT -240)])
-        pygame.draw.polygon(gridDisplay, MAGENTA, [(200, HEIGHT - 240), (230, HEIGHT -240), (230, HEIGHT -230),(200,HEIGHT -230)])
+        # Broken Rect
+        pygame.draw.polygon(gridDisplay, MAGENTA,
+                            [(200, HEIGHT - 280), (230, HEIGHT - 280), (230, HEIGHT - 270), (200, HEIGHT - 270)])
+        pygame.draw.polygon(gridDisplay, MAGENTA,
+                            [(200, HEIGHT - 270), (210, HEIGHT - 270), (210, HEIGHT - 240), (200, HEIGHT - 240)])
+        pygame.draw.polygon(gridDisplay, MAGENTA,
+                            [(200, HEIGHT - 240), (230, HEIGHT - 240), (230, HEIGHT - 230), (200, HEIGHT - 230)])
 
     def performAStar(self, start, end):
         """
@@ -122,28 +131,28 @@ class Graph:
         Input: Starting and ending node for the robot to browse.
         Output: Returns True or False to define if an optimal path can be found or not.
         """
-        
-        #Checking is start and end are in obstancle.
-        if self.isAnObstacle(start.i,start.j) and self.isAnObstacle(end.i, end.j):
+
+        # Checking is start and end are in obstancle.
+        if self.isAnObstacle(start.i, start.j) and self.isAnObstacle(end.i, end.j):
             print("Starting and ending point are inside the obstacle!")
             return
 
-        if self.isAnObstacle(start.i,start.j):
+        if self.isAnObstacle(start.i, start.j):
             print("Starting point is inside the obstacle!")
-            return 
+            return
         if self.isAnObstacle(end.i, end.j):
             print("Ending point is inside the obstacle!")
-            return 
-        
-        if self.isOutsideArena(start.i,start.j):
+            return
+
+        if self.isOutsideArena(start.i, start.j):
             print("Starting point is outside the arena!")
-            print("Starting point should be between (0,0) and (400,300)")
-            return 
-        
-        if self.isOutsideArena(end.i,end.j):
+            print("Starting point should be between (15,15) and (385,285)")
+            return
+
+        if self.isOutsideArena(end.i, end.j):
             print("Ending point is outside the arena!")
-            print("Ending point should be between (0,0) and (400,300)")
-            return 
+            print("Ending point should be between (15,15) and (385,285)")
+            return
 
         print("Finding path...")
         priorityQueue = []
@@ -154,12 +163,11 @@ class Graph:
             if self.isInTargetArea(currentNode.i, currentNode.j):
                 print("Found a path!")
                 return True
-            
+
             currentDistance = currentNode.costToCome
             neighbours = self.getNeighbours(currentNode)
             currentNode.neighbours = neighbours
             for neighbourNode, newDistance in neighbours.items():
-
                 neighbourNode.costToCome = currentDistance + newDistance
                 neighbourNode.cost = neighbourNode.costToCome + neighbourNode.costToGo
                 neighbourNode.parent = currentNode
@@ -177,9 +185,9 @@ class Graph:
         self.visited = {}
         priorityQueue = []
         heapq.heappush(priorityQueue, (start.cost, start))
-        pygame.draw.circle(gridDisplay, BLACK, [start.i,HEIGHT - start.j], 10)
-        pygame.draw.circle(gridDisplay, BLACK, [end.i,HEIGHT - end.j], 10)
-        pygame.display.update()  
+        pygame.draw.circle(gridDisplay, BLACK, [start.i, HEIGHT - start.j], 10)
+        pygame.draw.circle(gridDisplay, BLACK, [end.i, HEIGHT - end.j], 10)
+        pygame.display.update()
         while len(priorityQueue):
 
             currentNode = heapq.heappop(priorityQueue)
@@ -189,18 +197,18 @@ class Graph:
                 self.backTrack(currentNode)
                 print("Distance Required to reach from start to end is:", currentNode.costToCome)
                 return
-            
+
             currentDistance = currentNode.costToCome
             for neighbourNode, newDistance in currentNode.neighbours.items():
                 i = neighbourNode.i
                 j = neighbourNode.j
-                pygame.draw.line(gridDisplay, CYAN, [currentNode.i,HEIGHT - currentNode.j],  [i,HEIGHT - j],  2)
+                pygame.draw.line(gridDisplay, CYAN, [currentNode.i, HEIGHT - currentNode.j], [i, HEIGHT - j], 2)
                 pygame.display.update()
-                time.sleep(0.1)  
+                time.sleep(0.1)
 
                 heapq.heappush(priorityQueue, (neighbourNode.cost, neighbourNode))
 
-        return 
+        return
 
     def isInTargetArea(self, i, j):
         """
@@ -208,12 +216,10 @@ class Graph:
         Input: Current Node co-ordinates
         Output: Boolean
         """
-        if (i -  self.endI) **2 + (j- self.endJ)**2 - 100 <= 0:
+        if (i - self.endI) ** 2 + (j - self.endJ) ** 2 - 100 <= 0:
             return True
         else:
             return False
-
-
 
     def backTrack(self, child):
         """
@@ -227,90 +233,120 @@ class Graph:
             child = child.parent
         return True
 
-    def isinStartingCircle(self, start, i,j):
+    def isinStartingCircle(self, start, i, j):
         """
         Description: Checks if a point is in the starting circle from where the robot will start,
         Input: Point with co-ordinates (x,y)
         Output: True or False
         """
 
-        if (i - start.i) **2 + (j-start.j)**2 - 100 <= 0:
+        if (i - start.i) ** 2 + (j - start.j) ** 2 - 100 <= 0:
             return True
         else:
             return False
 
-    def isInStartingSquare(self, start, i,j):
+    def isInStartingSquare(self, start, i, j):
         """
         Description: Checks if a point is in the starting square from where the robot will start.
         Input: Point with co-ordinates (x,y)
         Output: True or False
         """
 
-        if (start.i-10 <= i <= start.i + 10) and (start.j -10 <= j <= start.j + 10):
+        if (start.i - 10 <= i <= start.i + 10) and (start.j - 10 <= j <= start.j + 10):
             return True
         else:
             return False
 
-    def isInCircle(self, x,y):
+    def isInCircle(self, x, y):
         """
-        Description: Checks if a point is in the circle. 
-        Input: Point with co-ordinates (x,y)
-        Output: True or False
-        """
-
-        if (x - 90) **2 + (y-70)**2 - 2500 > 0:
-            return False
-        else:
-            return True
-
-    def isInRectangle(self, x,y):
-        """
-        Description: Checks if a point is in the rotated rectangle. 
+        Description: Checks if a point is in the circle.
         Input: Point with co-ordinates (x,y)
         Output: True or False
         """
         clearance = 15
-        if (y + 1.42*x - 176.55 + clearance + 10) > 0  and (y - 0.7*x - 74.39 + clearance )  > 0 and (y - 0.7*x - 98.81 - clearance) < 0 and (y + 1.42*x - 438.06 - clearance - 10) < 0:
-            return True
-        else:
+        r = 35 + clearance
+        if (x - 90) ** 2 + (y - 70) ** 2 - r**2 >= 0:
             return False
+        else:
+            return True
 
-    def isInBrokenRectangle(self, x,y):
+    def isInRectangle(self, x, y):
         """
-        Description: Checks if a point is in the top rectangle. 
+        Description: Checks if a point is in the rotated rectangle.
         Input: Point with co-ordinates (x,y)
         Output: True or False
         """
         clearance = 15
-        if (x >= (200 - clearance) and x <= (210 + clearance) and y <= (280+ clearance) and y >=(230 - clearance)) or (x>= (210 - clearance) and x <= (230+ clearance) and y >=(270- clearance) and y <= (280+ clearance)) or (y >= (230- clearance) and y <= (240+ clearance) and x >= (210- clearance) and x <= (230+ clearance)):
+        circ1 = (x-48)**2 + (y-108)**2 <= clearance**2
+        circ2 = (x-170.876)**2 + (y-194.044)**2 <= clearance**2
+        circ3 = (x-159.4)**2 + (y-210.432)**2 <= clearance**2
+        circ4 = (x-36.524)**2 + (y-124.387)**2 <= clearance**2
+        side1 = 0.7*x - y + 74.39 <= 0
+        eside1 = 0.7*x - y + 74.39 - 1.22*clearance <= 0
+        side2 = -1.43*x - y + 176.55 <= 0
+        eside2 = -1.43*x - y + 176.55 - 1.74*clearance <= 0
+        side3 = 0.7*x - y + 98.81 >= 0
+        eside3 = 0.7*x - y + 98.81 + 1.22*clearance >= 0
+        side4 = -1.43*x - y + 438.06 >= 0
+        eside4 = -1.43*x - y + 438.06 + 1.74*clearance >= 0
+        rect1 = eside1 and side2 and eside3 and side4
+        rect2 = side1 and eside2 and side3 and eside4
+
+        if rect1 or rect2 or circ1 or circ2 or circ3 or circ4:
             return True
         else:
             return False
 
-    def isInEllipse(self, x,y):
+    def isInBrokenRectangle(self, x, y):
         """
-        Description: Checks if a point is in the Ellipse. 
+        Description: Checks if a point is in the top rectangle.
         Input: Point with co-ordinates (x,y)
         Output: True or False
         """
+        clearance = 15
 
-        a = 75
-        b = 45
+        rect1 = (y <= 280+clearance) and (y >= 270+clearance) and (x <= 230) and (x >= 200)
+        rect2 = (y >= 280) and (y >= 270) and (x <= 230+clearance) and (x >= 200-clearance)
+        rect3 = (y <= 270) and (y >= 240) and (x <= 210+clearance) and (x >= 200-clearance)
+        rect4 = (y <= 240+clearance) and (y >= 230-clearance) and (x <= 230) and (x >= 200)
+        rect5 = (y <= 240) and (y >= 230) and (x <= 230+clearance) and (x >= 200+clearance)
+        circ1 = (x-230)**2 + (y-280)**2 <= clearance**2
+        circ2 = (x-200)**2 + (y-280)**2 <= clearance**2
+        circ3 = (x-230)**2 + (y-270)**2 <= clearance**2
+        circ4 = (x-230)**2 + (y-240)**2 <= clearance**2
+        circ5 = (x-230)**2 + (y-230)**2 <= clearance**2
+        circ6 = (x-200)**2 + (y-230)**2 <= clearance**2
+
+        if rect1 or rect2 or rect3 or rect4 or rect5 or circ1 or circ2 or circ3 or circ4 or circ5 or circ6:
+            return True
+        else:
+            return False
+
+    def isInEllipse(self, x, y):
+        """
+        Description: Checks if a point is in the Ellipse.
+        Input: Point with co-ordinates (x,y)
+        Output: True or False
+        """
+        clearance = 15
+        a = 60+clearance
+        b = 20+clearance
         h = 246
         k = 145
-        if ((math.pow((x - h), 2) / math.pow(a, 2)) + (math.pow((y - k), 2) / math.pow(b, 2))) < 1:
+        if ((math.pow((x - h), 2) / math.pow(a, 2)) + (math.pow((y - k), 2) / math.pow(b, 2))) <= 1:
             return True
         else:
             return False
 
-    def isAnObstacle(self,x,y):
+    def isAnObstacle(self, x, y):
         """
         Description: Checks if the point (x,y) is inside an obstacle or not.
         Input: Point with co-ordinates (x,y)
         Output: True or False
         """
 
-        return self.isInEllipse(x,y) or self.isInBrokenRectangle(x,y) or self.isInCircle(x,y) or self.isInRectangle(x,y)
+        return self.isInEllipse(x, y) or self.isInBrokenRectangle(x, y) or self.isInCircle(x, y) or self.isInRectangle(
+            x, y)
 
     def isOutsideArena(self, x, y):
         """
@@ -318,7 +354,10 @@ class Graph:
         Input: Point with co-ordinates (x,y)
         Output: True or False
         """
-        return True if x < 0 or y < 0 or x > 400 or y > 300 else False
+        clearance = 15
+
+        return True if x < clearance or y < clearance or x > 400-clearance or y > 300-clearance else False
+
 
 x1 = int(input("Enter the x coordiante of the starting point: "))
 y1 = int(input("Enter the y coordiante of the starting point: "))
@@ -331,50 +370,50 @@ print("#############################################")
 
 MAGNITUDE = int(input("Enter the step size of the robot:  "))
 
-#############################################           
-#Algorithm Driver  
+#############################################
+# Algorithm Driver
 end = Node(x2, y2, x2, y2, 0)
 start = Node(x1, y1, x2, y2, thetaStart)
 start.costToCome = 0
 robot = Graph(start, end, MAGNITUDE)
 path = []
 
-#Check if path can be found
+# Check if path can be found
 if robot.performAStar(start, end):
     pass
-    pygame.init() #Setup Pygame
+    pygame.init()  # Setup Pygame
     gridDisplay = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("A* Algorithm - Rigid Robot")
     exiting = False
     clock = pygame.time.Clock()
     grid = [[0 for j in range(HEIGHT)] for i in range(WIDTH)]
-    canvas = Graph(start, end, MAGNITUDE) #Create Canvas
+    canvas = Graph(start, end, MAGNITUDE)  # Create Canvas
     canvas.generateGraph()
     robot.visualizeAStar(start, end)
     path.reverse()
 else:
-    #No Path Found
+    # No Path Found
     exiting = True
 
 #############################################
-#Running the simulation in loop
+# Running the simulation in loop
 
 while not exiting:
-    for event in pygame.event.get():  
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            exiting = True  
+            exiting = True
 
-    #Visualizing the final path
+            # Visualizing the final path
     for index in range(len(path)):
         x, y = path[index]
         if index != 0:
-            pygame.draw.line(gridDisplay, MAGENTA, [prevX,HEIGHT - prevY],  [x,HEIGHT - y],  2)
+            pygame.draw.line(gridDisplay, MAGENTA, [prevX, HEIGHT - prevY], [x, HEIGHT - y], 2)
             pygame.display.update()
 
         time.sleep(.1)
         prevX = x
         prevY = y
-        
+
     clock.tick(2000)
     pygame.display.flip()
     exiting = True
