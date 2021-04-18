d8ddbc475a905061376b9288c1c76b0411499510
Sameer <sameer@umd.edu>
2021-04-18 17:42:18 -0400

    Removed Unnecesary Files

--- Project5/RRT/euclidian.py
+++
@@ -1,8 +0,0 @@
-import math
-def getEuclidianDistance():
-    parentX = 10
-    parentY = 10
-    currentX = 20
-    currentY = 20   
-    return math.sqrt((parentX - currentX) ** 2 + (parentY - currentY) ** 2)
-print(getEuclidianDistance())
